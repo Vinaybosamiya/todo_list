bca70052495aeeb6ce11f4a67323e7b2ab3a59d6
Vinaybosamiya <151708612+Vinaybosamiya@users.noreply.github.com>
2025-09-18 22:56:20 +0530

    Complete the todo lilst

--- TodoList/src/components/navbar.jsx
+++ TodoList/src/components/navbar.jsx
@@ -7,10 +7,10 @@ const navbar = () => {
                 <span className='font-bold text-xl '>Krishna Vibes</span>
             </div>
             <ul className="flex items-center gap-8 mx-7 p-4 cursor-pointer">
-                <li><span className='rounded-2xl px-2 py-1  font-bold  text-[15px] hover:text-black-t hover:bg-white-b  '>Home</span></li>
-                <li><span className='rounded-2xl px-2 py-1  font-bold  text-[15px] hover:text-black-t hover:bg-white-b  '>About</span></li>
+                {/* <li><span className='rounded-2xl px-2 py-1 sm:font-[12px] sm:gap-1 font-bold  text-[15px] hover:text-black-t hover:bg-white-b  '>Home</span></li>
+                <li><span className='rounded-2xl px-2 py-1 sm:font-[12px] sm:gap-1 font-bold  text-[15px] hover:text-black-t hover:bg-white-b  '>About</span></li> */}
                 
-                <li><span className='rounded-2xl px-2 py-1  font-bold  text-[15px] hover:text-black-t hover:bg-white-b  '>Your Task</span> </li>
+                <li><span className='rounded-2xl px-2 py-1 font-bold  text-[15px] hover:text-black-t hover:bg-white-b  '>Your Task</span> </li>
 
             </ul>
 
